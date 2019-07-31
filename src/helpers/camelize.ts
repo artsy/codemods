6e93857c2e7515e69046411b5786ec0e717c2529
Eloy Durán <eloy.de.enige@gmail.com>
2019-07-31 14:06:03 +0200

    [camelize] Don't apply special cases to first part

--- src/helpers/camelize.ts
+++ src/helpers/camelize.ts
@@ -2,27 +2,30 @@ export function camelize(input: string) {
   if (input.startsWith("__") || !input.includes("_")) {
     return input
   }
-  const components = input.split("_").map(c => {
-    switch (c) {
-      case "id":
-        return "ID"
-      case "url":
-        return "URL"
-      case "usd":
-        return "USD"
-      case "utc":
-        return "UTC"
-      case "md":
-        return "MD"
-      case "jwt":
-        return "JWT"
-      default:
-        return c
-    }
-  })
+  const components = input.split("_")
   return [
     components[0],
     components
+      .map(c => {
+        switch (c) {
+          case "id":
+            return "ID"
+          case "ids":
+            return "IDs"
+          case "url":
+            return "URL"
+          case "usd":
+            return "USD"
+          case "utc":
+            return "UTC"
+          case "md":
+            return "MD"
+          case "jwt":
+            return "JWT"
+          default:
+            return c
+        }
+      })
       .slice(1)
       .map(c => c[0].toUpperCase() + c.substring(1))
       .join(""),
